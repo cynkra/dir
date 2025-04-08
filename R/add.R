96c8758d25b0824cc67eef8a0e5912bfc93feb25
Antoine Fabri <antoine.fabri@gmail.com>
2025-04-08 22:33:16 +0200

    Initial commit

---
+++ R/add.R
@@ -0,0 +1,94 @@
+
+#' Add Folders to a R package
+#'
+#' @description
+#'
+#' `add()` allows you to store your code in a nested folder structure, rather than
+#' solely in the "R" folder. To do so:
+#'
+#' * Have a call to `dir::add()` in your `.onLoad()` definition as shown
+#'  in the examples at the bottom of this page.
+#' * Call `devtools::load_all()` in your workflow, and never `devtools::document()`.
+#' Indeed `load_all()` will document everything through `dir::add()`, and
+#' `devtools::document()` would drop some ".Rd" files.
+#'
+#' Files and folders starting with "_" in your added folders will be ignored by the process.
+#'
+#' @section How does it work?:
+#'
+#' `dir::add()` will:
+#'
+#' * Add the 'dir' package to your "Suggests" dependencies in the "DESCRIPTION" file.
+#' * Add your added folders to ".Rbuidignore"
+#' * Load the code from your added folders into the `sysdata.rda` file in the R folder.
+#' * Load the objects into the session
+#' * Document everything, like `devtools::document()`
+#'
+#' @param ... Folders to add. You might provide strings, vectors or lists, they
+#'   will be flattened into a vector.
+#' @param .recursive Boolean. Whether to add folders under the added folders recursively.
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#' .onLoad <- function(libname, pkgname) {
+#'   # the condition makes sure this is run only during development
+#'   if (Sys.getenv("DEVTOOLS_LOAD") == pkgname) dir::add("new_top_level_folder", "maybe_another_one")
+#' }
+#' }
+add <- function(..., .recursive = TRUE) {
+  # setup ======================================================================
+  usethis::use_package("dir", "Suggests")
+  withr::local_dir(here::here())
+  dirs <- sort(unlist(list(...)))
+  dirs_exist <- sapply(dirs, dir.exists)
+  if (!all(dirs_exist)) {
+    msg <- "Additional dirs must exist"
+    info <- sprintf("Not found: %s", toString(shQuote(dirs[!dirs_exist])))
+    rlang::abort(c(msg, x = info))
+  }
+  files <- unlist(lapply(dirs, list.files, recursive = .recursive, full.names = TRUE, pattern = "\\.[Rr]$"))
+  files <- grep("/_", files, invert = TRUE, value = TRUE)
+
+  # update .Rbuildignore  ======================================================
+  # remove previous lines
+  build_ignore <-
+    if (file.exists(".Rbuildignore")) readLines(".Rbuildignore") else character()
+  remove <-
+    cumsum(build_ignore %in% c("# dir::add() start", "# dir::add() end")) == 1
+  build_ignore <- build_ignore[!remove]
+  # add new lines
+  build_ignore <- c(
+    build_ignore,
+    "# dir::add() start",
+    paste0("^", dirs, "$"),
+    "# dir::add() end"
+  )
+  # save
+  writeLines(build_ignore, ".Rbuildignore")
+
+  # update sysdata.rda =========================================================
+  e <- new.env()
+  for (file in files) source(file, local = e)
+  ns <- topenv(parent.frame())
+  # set environment of functions and env formulas to ns
+  objs <- eapply(e, function(x) {
+    if (is.function(x) || rlang::is_formula("formula")) {
+      environment(x) <- ns
+    }
+    x
+  })
+  save(list = names(e), file = "R/sysdata.rda", envir = as.environment(objs), compress = "bzip2", version = 3, ascii = FALSE)
+
+  # update the namespace with these values =====================================
+  # sysdata.rda was already read when we call add() so we move them to the namespace manually
+  list2env(objs, ns)
+
+  # document ===================================================================
+  renamed_files <- file.path("R", gsub("/", "--", files))
+  file.rename(files, renamed_files)
+  on.exit(file.rename(renamed_files, files))
+  # load_code is set so the code is not reloaded after reoxygize()
+  roxygen2::roxygenize(".", load_code = function(x) ns)
+  pkgload::dev_topic_index_reset(".")
+}
